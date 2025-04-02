2d86e0350d68f4c184185e1421fdc8d8cd6107a2
JoakimDev <joakgomcoas@gmail.com>
2025-04-02 16:25:27 +0200

    test

--- Code Arduino/Capteur_graphite/Capteur_graphite_2/Capteur_graphite_2.ino
+++ Code Arduino/Capteur_graphite/Capteur_graphite_2/Capteur_graphite_2.ino
@@ -1,75 +1,121 @@
-// Definition des pins
+// Définition des pins
+#define encoder0PinA  2  // CLK Output A
+#define encoder0PinB  4  // DT Output B
+#define Switch 5  // Bouton de l'encodeur
 
-// Encodeur Rotatoire //
-#define encoder0PinA  2  //CLK Output A Do not use other pin for clock as we are using interrupt
-#define encoder0PinB  4  //DT Output B
-#define Switch 5 // Switch connection if available
+#include <Adafruit_SSD1306.h>
+
+#define OLED_WIDTH 128
+#define OLED_HEIGHT 64
+#define OLED_RESET -1
+#define OLED_I2C_ADDR 0x3C
+
+Adafruit_SSD1306 ecranOLED(OLED_WIDTH, OLED_HEIGHT, &Wire, OLED_RESET);
+
+// Variables globales
 volatile int encoder0Pos = 0;
 volatile bool encoderChanged = false;
 
-// Ecran OLED
-#include <Adafruit_SSD1306.h>
-#define nombreDePixelsEnLargeur 128         // Taille de l'écran OLED, en pixel, au niveau de sa largeur
-#define nombreDePixelsEnHauteur 64          // Taille de l'écran OLED, en pixel, au niveau de sa hauteur
-#define brocheResetOLED         -1          // Reset de l'OLED partagé avec l'Arduino (d'où la valeur à -1, et non un numéro de pin)
-#define adresseI2CecranOLED     0x3C        // Adresse de "mon" écran OLED sur le bus i2c (généralement égal à 0x3C ou 0x3D)
-Adafruit_SSD1306 ecranOLED(nombreDePixelsEnLargeur, nombreDePixelsEnHauteur, &Wire, brocheResetOLED);
+int menuState = 0;  // 0 = Menu principal, 1 = Config, 2 = Mesure
+int selection = 0;  // Position de sélection (0, 1 ou 2)
+
+bool lastButtonState = HIGH;
+bool buttonPressed = false;
 
 void setup() {
   Serial.begin(9600);
 
-  // Encodeur Rotatoire
-  pinMode(encoder0PinA, INPUT); 
-  pinMode(encoder0PinB, INPUT); 
-  attachInterrupt(digitalPinToInterrupt(encoder0PinA), doEncoder, CHANGE);
-  // Ecran OLED 
-  if(!ecranOLED.begin(SSD1306_SWITCHCAPVCC, adresseI2CecranOLED))
-    while(1);                               // Arrêt du programme (boucle infinie) si échec d'initialisation
-  afficherMenu(); 
-}
+  pinMode(encoder0PinA, INPUT);
+  pinMode(encoder0PinB, INPUT);
+  pinMode(Switch, INPUT_PULLUP);
 
+  attachInterrupt(digitalPinToInterrupt(encoder0PinA), doEncoder, CHANGE);
+
+  if (!ecranOLED.begin(SSD1306_SWITCHCAPVCC, OLED_I2C_ADDR)) {
+    while (1);
+  }
+
+  afficherMenu();
+}
 
 void loop() {
-  doEncoder();
-  if (encoderChanged) {  // Si la valeur de l'encodeur a changé
+  if (encoderChanged) {
     encoderChanged = false;
-   afficherMenu();
+    afficherMenu();
   }
+
+  detecterAppuiBouton();
 }
 
+void detecterAppuiBouton() {
+  bool buttonState = digitalRead(Switch);
+
+  if (buttonState == LOW && lastButtonState == HIGH) {
+    delay(50);
+    if (digitalRead(Switch) == LOW) {
+      buttonPressed = true;
+    }
+  }
+
+  if (buttonState == HIGH && lastButtonState == LOW) {
+    if (buttonPressed) {
+      changerMenu();
+      buttonPressed = false;
+    }
+  }
+
+  lastButtonState = buttonState;
+}
 
 void afficherMenu() {
   ecranOLED.clearDisplay();
-  ecranOLED.setTextSize(3);
-  if (encoder0Pos % 2 == 0) {
-      ecranOLED.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
-      ecranOLED.setCursor(20, 10); 
-      ecranOLED.println("Config");
-      ecranOLED.setTextColor(SSD1306_WHITE, SSD1306_BLACK);
-      ecranOLED.setCursor(20, 40); 
-      ecranOLED.println("Mesure");
+  ecranOLED.setTextSize(2);
+
+  if (menuState == 0) {  // Menu principal
+    afficherOption("Config", 0);
+    afficherOption("Mesure", 1);
+  } else if (menuState == 1) {  // Sous-menu "Config"
+    afficherOption("Option A", 0);
+    afficherOption("Option B", 1);
+    afficherOption("Retour", 2);
+  } else if (menuState == 2) {  // Sous-menu "Mesure"
+    afficherOption("Graphite", 0);
+    afficherOption("Flex", 1);
+    afficherOption("Retour", 2);
+  }
+
+  ecranOLED.display();
+}
+
+void afficherOption(const char *texte, int position) {
+  if (selection % 3 == position) {
+    ecranOLED.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
   } else {
-      ecranOLED.setTextColor(SSD1306_WHITE, SSD1306_BLACK);
-      ecranOLED.setCursor(20, 10); 
-      ecranOLED.println("Config");
-      ecranOLED.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
-      ecranOLED.setCursor(20, 40); 
-      ecranOLED.println("Mesure");
+    ecranOLED.setTextColor(SSD1306_WHITE, SSD1306_BLACK);
+  }
+  ecranOLED.setCursor(20, 10 + (position * 20));
+  ecranOLED.println(texte);
+}
+
+void changerMenu() {
+  if (menuState == 0) {  // Menu principal
+    menuState = (selection == 0) ? 1 : 2;
+  } else {  // Sous-menus
+    if (selection == 2) {
+      menuState = 0;  // Retour au menu principal
     }
-    ecranOLED.display();
+  }
+
+  selection = 0;  // Réinitialiser la sélection
+  afficherMenu();
 }
 
-
-// Encodeur Rotatoire : Ajustement du cran de l'encodeur
 void doEncoder() {
-  if (digitalRead(encoder0PinA)==HIGH && digitalRead(encoder0PinB)==HIGH) {
-    encoder0Pos--;
-  } else if (digitalRead(encoder0PinA)==HIGH && digitalRead(encoder0PinB)==LOW) {
-    encoder0Pos++;
-  } 
+  if (digitalRead(encoder0PinA) == HIGH && digitalRead(encoder0PinB) == HIGH) {
+    selection = (selection + 1) % 3;
+  } else if (digitalRead(encoder0PinA) == HIGH && digitalRead(encoder0PinB) == LOW) {
+    selection = (selection - 1 + 3) % 3;
+  }
+
   encoderChanged = true;
-  Serial.println(encoder0Pos);
 }
-
-
-
